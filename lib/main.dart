36943c46146d6e46b61011859dd4c51b89e4319b
Alan José <alanjsdelima@gmail.com>
2025-06-23 13:18:44 -0300

    Adding Todo-List and edit feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,152 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<String> _todos = [];
+
+  void _addTodo() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String newTodo = "";
+        return AlertDialog(
+          title: Text("Add a new To-Do"),
+          content: TextField(
+            onChanged: (value) {
+              newTodo = value;
+            },
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (newTodo.isNotEmpty) {
+                    _todos.add(newTodo);
+                  }
+                  Navigator.pop(context);
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Colors.lightBlue,
+                ),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+              ),
+              child: Text("Add"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "My To-Do List",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: ListView.builder(
+        itemCount: _todos.length,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 4,
+            margin: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+            child: ListTile(
+              title: Text(_todos[index], style: TextStyle(fontSize: 18)),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          String targetTodo = _todos[index];
+                          String updatedTodo = "";
+                          TextEditingController controller =
+                              TextEditingController(text: targetTodo);
+                          return AlertDialog(
+                            title: Text('Update To-Do'),
+                            content: TextField(
+                              controller: controller,
+                              onChanged: (value) {
+                                updatedTodo = value;
+                              },
+                            ),
+                            actions: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    if (updatedTodo.isNotEmpty) {
+                                      _todos[index] = updatedTodo;
+                                    }
+                                    Navigator.pop(context);
+                                  });
+                                },
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                        Colors.lightBlue,
+                                      ),
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                        Colors.white,
+                                      ),
+                                ),
+                                child: Text('Update To-Do'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      setState(() {
+                        _todos.removeAt(index);
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodo,
+        backgroundColor: Colors.lightBlue,
+        child: Icon(Icons.add, color: Colors.white),
       ),
     );
   }
